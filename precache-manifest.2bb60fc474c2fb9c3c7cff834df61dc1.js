187baedd8766003289873ccff7e09a999b7d380c
AreonL <areon.lundkvist@gmail.com>
2024-02-12 23:41:42 +0100

    Updates

---
+++ precache-manifest.2bb60fc474c2fb9c3c7cff834df61dc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e8d1fbfd6fc1f0f4eb638d2e118bb1c",
+    "url": "/github-website/index.html"
+  },
+  {
+    "revision": "5982d34947dda9b77212",
+    "url": "/github-website/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "1f5929bab9e74fc2d234",
+    "url": "/github-website/static/js/2.a71b1702.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/github-website/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "5982d34947dda9b77212",
+    "url": "/github-website/static/js/main.c52684ed.chunk.js"
+  },
+  {
+    "revision": "e517ab20559735770302",
+    "url": "/github-website/static/js/runtime~main.7a015c0f.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/github-website/static/media/logo.06e73328.svg"
+  }
+]);
